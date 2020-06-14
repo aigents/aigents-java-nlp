a3684dfd59a32ae31a7a3fe29605a7cec0148559
rvignav <rvignav@gmail.com>
2020-06-13 18:56:48 -0700

    Add rule field to Word class

--- src/org/aigents/nlp/lg/Word.java
+++ src/org/aigents/nlp/lg/Word.java
@@ -4,11 +4,16 @@ import java.util.*;
 import java.io.*;
 
 public class Word {
-	String word, classname, subscript;
+	String word, classname, subscript, rule;
 	
-	public Word(String word, String classname, String subscript) {
+	public Word(String word, String classname, String subscript, String rule) {
 		this.word = word;
 		this.classname = classname;
 		this.subscript = subscript;
+		this.rule = rule;
+	}
+	
+	public void updateRule(String rule) {
+		this.rule = rule;
 	}
 }
