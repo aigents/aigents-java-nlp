a53f862ead3fcdb5b7c57f1e95e71f2f66e6f44e
Vignav Ramesh <33817654+rvignav@users.noreply.github.com>
2020-07-06 12:01:29 -0700

    Update Word.java

--- src/org/aigents/nlp/lg/Word.java
+++ src/org/aigents/nlp/lg/Word.java
@@ -25,37 +25,52 @@
 package org.aigents.nlp.lg;
 
 public class Word {
-  private String word;
-  private Rule rule;
-  
-  public Word(String word, Rule rule) {
-    this.word = word;
-    this.rule = rule;
-  }
-  
-  public Word(String word) {
-    this.word = word;
-    rule = new Rule();
-  }
-  
-  public void addRule(String rule) {
-    this.rule.addWord(rule);
-  }
-  
-  public boolean containsRule(String rule) {
-    return this.rule.getWords().contains(rule);
-  }
-  
-  public void updateRule(Rule rule) {
-    this.rule = rule;
-  }
-  
-  public String getWord() { return word;  }
-  
-  public Rule getRule() { return rule;  }
-  
-  @Override
-  public String toString() {
-    return word;
-  }
+	private String word, subscript;
+	private Rule rule;
+	
+	public Word(String word, Rule rule) {
+		this.word = word;
+		this.rule = rule;
+		this.subscript = "";
+	}
+	
+	public Word(String word, Rule rule, String subscript) {
+		this.word = word;
+		this.rule = rule;
+		this.subscript = subscript;
+	}
+	
+	public Word(String word) {
+		this.word = word;
+		rule = new Rule();
+	}
+	
+	public Word(String word, String subscript) {
+		this.word = word;
+		this.subscript = subscript;
+		rule = new Rule();
+	}
+	
+	public void addRule(String rule) {
+		this.rule.addWord(rule);
+	}
+	
+	public boolean containsRule(String rule) {
+		return this.rule.getWords().contains(rule);
+	}
+	
+	public void updateRule(Rule rule) {
+		this.rule = rule;
+	}
+	
+	public String getWord() {	return word;	}
+	
+	public Rule getRule() {		return rule;	}
+	
+	public String getSubscript() {	return subscript;	}
+	
+	@Override
+	public String toString() {
+		return word + (subscript.equals("")? "" : "." + subscript) + ": " + rule;
+	}
 }
